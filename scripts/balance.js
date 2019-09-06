1b4ffb9b49bae634608602f34d3ee16c761540eb
serkankayaa <srknnkaya@gmail.com>
2019-09-06 16:59:54 +0300

    added images

--- scripts/balance.js
+++ scripts/balance.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
     var balances = [];
+    var game = game1;
 
-    
+    prepareBalances(game);
+
+    function prepareBalances(game) {
+        const balanceCount = game.leftChamber.length;
+        const balanceHtml = "";
+
+        for (let i = 0; i < balanceCount; i++) {
+        }
+    }
 });
