13f4dd37f1186cdc08252de223652b4fbc79c247
serkankayaa <srknnkaya@gmail.com>
2019-09-06 12:17:37 +0300

    added games.js

---
+++ scripts/games.js
@@ -0,0 +1,22 @@
+var balanceObject = {
+    
+}
+
+var game1 = {
+    name: 'game1',
+    triangleValue: 5,
+    rectValue: 3,
+    circleValue: 2,
+    leftChamber: [
+        { triangleCount: 1, rectCount: 1, circleCount: 0 },
+        { triangleCount: 2, rectCount: 0, circleCount: 1 },
+        { triangleCount: 0, rectCount: 0, circleCount: 2 },
+        { triangleCount: 1, rectCount: 1, circleCount: 1 },
+    ],
+    rightChamber: [
+        { triangleCount: 1, rectCount: 0, circleCount: 2 },
+        { triangleCount: 1, rectCount: 2, circleCount: 0 },
+        { triangleCount: 1, rectCount: 1, circleCount: 1 },
+        { triangleCount: 0, rectCount: 1, circleCount: 1 },
+    ],
+}
