fbbad960b67ab60c8c0578017e4c6060d7320e4d
serkankayaa <srknnkaya@gmail.com>
2019-09-09 10:37:57 +0300

    added shapes

--- scripts/balance.js
+++ scripts/balance.js
@@ -1,14 +1,32 @@
 $(document).ready(function () {
     var balances = [];
     var game = game1;
+    var balanceCol = 'bln col';
 
     prepareBalances(game);
 
     function prepareBalances(game) {
-        const balanceCount = game.leftChamber.length;
-        const balanceHtml = "";
+        var balanceCount = game.leftChamber.length;
+        var balanceHtml = "";
 
-        for (let i = 0; i < balanceCount; i++) {
+        for (var i = 0; i < balanceCount; i++) {
+            balanceHtml = "<div class='col blnCol'>";
+            balanceHtml += `<div id='balance${i}' class='blnShape'>`;
+            balanceHtml += `<div class="hanger"></div>
+            <div class="pendulum"></div>
+            <div class="ropeLeft"></div>
+            <div class="ropeRight"></div>
+            <div class="boxLeft"></div>
+            <div class="boxRight"></div></div></div>`;
+
+            $(".bln").append(balanceHtml);
         }
+
+        $(".blnShape").each(function () {
+            var blnShapeId = ($(this).attr("id"));
+
+            balances.push("#" + blnShapeId);
+            balances.sort();
+        });
     }
 });
