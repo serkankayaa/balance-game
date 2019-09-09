099e09b66a03129e95be1898f7b00f4a6bf834e1
serkankayaa <srknnkaya@gmail.com>
2019-09-09 12:28:39 +0300

    edited shapes

--- scripts/balance.js
+++ scripts/balance.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
     var balances = [];
     var game = game1;
-    var balanceCol = 'bln col';
+    var shapeCount = 5;
 
     prepareBalances(game);
+    addStyles();
 
     function prepareBalances(game) {
         var balanceCount = game.leftChamber.length;
         var balanceHtml = "";
+        var shapeHtml = "";
 
         for (var i = 0; i < balanceCount; i++) {
             balanceHtml = "<div class='col blnCol'>";
@@ -28,5 +30,62 @@ $(document).ready(function () {
             balances.push("#" + blnShapeId);
             balances.sort();
         });
+
+        shapeHtml += `<div class="row shapes">
+        <div class="col circles"></div>
+        <div class="col squares"></div>
+        <div class="col triangles"></div>
+        </div>`;
+
+        $(".bln").append(shapeHtml);
+
+        prepareCircle(shapeCount, game1.circleValue);
+        prepareSquare(shapeCount, game1.rectValue);
+        prepareTriangle(shapeCount, game1.rectValue);
+    }
+
+    $(".dragShape").draggable({
+        containment: 'window',
+        stack: '.dragShape',
+        snap: '.boxLeft, .boxRight',
+        snapMode: 'inner',
+        snapTolerance: 10,
+    });
+
+    function addStyles() {
+        $('.shapes').css({
+            'margin-top': '20%',
+            'margin-right' : '4px',
+        });
+    }
+
+    function prepareCircle(shapeCount, value) {
+        var shapeHtml = "";
+        var shapeId = "circleShape"
+
+        for (let i = 0; i < shapeCount; i++) {
+            shapeHtml = `<div id=${shapeId} value=${value} class="dragShape" style="margin-left:3px; position:absolute"></div>`;
+            $(".circles").append(shapeHtml);
+        }
+    }
+
+    function prepareSquare(shapeCount, value) {
+        var shapeHtml = "";
+        var shapeId = "squareShape"
+
+        for (let i = 0; i < shapeCount; i++) {
+            shapeHtml = `<div id=${shapeId} value=${value} class="dragShape" style="margin-left:3px; position:absolute"></div>`;
+            $(".squares").append(shapeHtml);
+        }
+    }
+
+    function prepareTriangle(shapeCount, value) {
+        var shapeHtml = "";
+        var shapeId = "triangleShape"
+
+        for (let i = 0; i < shapeCount; i++) {
+            shapeHtml = `<div id=${shapeId} value=${value} class="dragShape" style="margin-left:3px; position:absolute"></div>`;
+            $(".triangles").append(shapeHtml);
+        }
     }
 });
